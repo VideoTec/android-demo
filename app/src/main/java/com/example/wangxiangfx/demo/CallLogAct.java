545d3a4f1ae6a7328145244539319255b2853bd2
wx <wx@gmail.com>
2016-03-29 21:45:37 +0800

    update calllog code

--- app/src/main/java/com/example/wangxiangfx/demo/CallLogAct.java
+++ app/src/main/java/com/example/wangxiangfx/demo/CallLogAct.java
@@ -1,5 +1,6 @@
 package com.example.wangxiangfx.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -16,5 +17,15 @@ public class CallLogAct extends AppCompatActivity {
         mAdapter = new CallLogListItemAdapter(this, DemoACT.sCallItems);
         mAllCallsListView = (ListView)findViewById(R.id.all_call_log_list);
         mAllCallsListView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
